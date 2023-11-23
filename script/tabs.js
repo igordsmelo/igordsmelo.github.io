2644aa5da31560070bb0b5982af73a9e0a264f16
Igor <igordsmelo@gmail.com>
2023-11-23 16:45:04 -0300

    updates. and more to come.

--- script/tabs.js
+++ script/tabs.js
@@ -2,28 +2,40 @@ $(document).ready(function(){
     $("#tabs-home").load("pages/home.html", function() {
         setTimeout(masonry, 1); // Call your function here
     });
-    $("#tabs-art").load("pages/art.html", function() {
+    $("#tabs-art").load("pages/portfolio.html", function() {
       portfolioFiles("data/pages.json", 'art');
-    });
-    $("#tabs-design").load("pages/design.html", function() {
-      portfolioFiles("data/pages.json", 'design');
-    });
-    $("#tabs-dev").load("pages/dev.html", function() {
-      portfolioFiles("data/pages.json", 'dev');
     });
     $("#tabs-contact").load("pages/contact.html", function() {
       portfolioFiles("data/pages.json", 'contact');
     });
-    $("#tab-art").load("pages/test.html", function() {
-      portfolioFiles("data/pages.json", 'art');
-    });
-    $("#tab-design").load("pages/test.html", function() {
+    $("#tabs-design").load("pages/portfolio.html", function() {
       portfolioFiles("data/pages.json", 'design');
     });
-    $("#tab-dev").load("pages/test.html", function() {
+    $("#tabs-dev").load("pages/portfolio.html", function() {
       portfolioFiles("data/pages.json", 'dev');
     });
-    $("#tab-contact").load("pages/test.html", function() {
-      portfolioFiles("data/pages.json", 'contact');
-    });
 });
+
+// $(document).ready(function(){
+//   $("#tabs-home").load("pages/projects.html", function() {
+//       setTimeout(masonry, 1); // Call your function here
+//   });
+//   $("#tabs-art").load("pages/art.html", function() {
+//     portfolioFiles("data/pages.json", 'art');
+//   });
+//   $("#tabs-design").load("pages/design.html", function() {
+//     portfolioFiles("data/pages.json", 'design');
+//   });
+//   $("#tabs-dev").load("pages/dev.html", function() {
+//     portfolioFiles("data/pages.json", 'dev');
+//   });
+//   $("#tabs-contact").load("pages/contact.html", function() {
+//     portfolioFiles("data/pages.json", 'contact');
+//   });
+//   $("#tab-art").load("pages/test.html", function() {
+//     portfolioFiles("data/pages.json", 'art');
+//   });
+//   $("#tab-design").load("pages/test.html", function() {
+//     portfolioFiles("data/pages.json", 'art');
+//   });
+// });
