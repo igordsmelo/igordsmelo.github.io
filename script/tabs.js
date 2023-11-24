e3650644d1eeb950cb5967505686c6525000b0c6
Igor <igordsmelo@gmail.com>
2023-11-24 18:07:01 -0300

    improvements on layout and overall design.

--- script/tabs.js
+++ script/tabs.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#tabs-home").load("pages/portfolio.html", function() {
+    $("#tabs-home").load("pages/home.html", function() {
         setTimeout(masonry, 1); // Call your function here
     });
     $("#tabs-art").load("pages/portfolio.html", function() {
@@ -15,27 +15,3 @@ $(document).ready(function(){
       portfolioFiles("data/pages.json", 'dev');
     });
 });
-
-// $(document).ready(function(){
-//   $("#tabs-home").load("pages/projects.html", function() {
-//       setTimeout(masonry, 1); // Call your function here
-//   });
-//   $("#tabs-art").load("pages/art.html", function() {
-//     portfolioFiles("data/pages.json", 'art');
-//   });
-//   $("#tabs-design").load("pages/design.html", function() {
-//     portfolioFiles("data/pages.json", 'design');
-//   });
-//   $("#tabs-dev").load("pages/dev.html", function() {
-//     portfolioFiles("data/pages.json", 'dev');
-//   });
-//   $("#tabs-contact").load("pages/contact.html", function() {
-//     portfolioFiles("data/pages.json", 'contact');
-//   });
-//   $("#tab-art").load("pages/test.html", function() {
-//     portfolioFiles("data/pages.json", 'art');
-//   });
-//   $("#tab-design").load("pages/test.html", function() {
-//     portfolioFiles("data/pages.json", 'art');
-//   });
-// });
